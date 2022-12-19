39c0691d3117edc8aee5a36b0a35fa45a8f1ce6a
s06a <91134347+S06A@users.noreply.github.com>
2022-12-19 18:12:10 +0330

    added multiplication game

---
+++ multiplication_game.py
@@ -0,0 +1,33 @@
+"""A simple game which asks you multiplication questions,
+which gets harder till the answers are correct
+"""
+import random
+
+
+def multiplication_game(length):
+    """Asks multiplication of two numbers.
+
+    length is integer
+    num1 is integer
+    num2 is integer
+    question is string
+    answer is integer
+    returns question and answer
+    """
+    num1 = int(random.random() * 10**length)
+    num2 = int(random.random() * 10**length)
+    question = str(num1) + 'X' + str(num2)
+    answer = str(num1 * num2)
+
+    return question, answer
+
+
+if __name__ == "__main__":
+
+    question, answer = multiplication_game(2)
+
+    while answer == input(question):
+        print('good job')
+        question, answer = multiplication_game(2)
+
+    print('maybe next time')
